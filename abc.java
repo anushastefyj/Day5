5c849dbcfdbfcbb722b9e37a6f6e3a26727e3b60
anushastefyj <24hr5a3001@mtieat.org>
2024-12-06 13:37:46 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class abc{
+    public static void main(String args[])
+    {
+        for(int i=1;i<6;i++)
+        {
+        for(int j=i;j<6;j++)
+        {
+        System.out.print((char)(j+64)+" ");
+        }
+        System.out.println();
+        }
+    }
+}
